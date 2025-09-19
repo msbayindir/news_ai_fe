0383dfa2c9f9a688085e8246aa8775f44a0acaf2
muhammedbayindir <OttomanState3@hotmail.com>
2025-09-19 13:53:15 +0300

    .

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://brief-jaybird-allowed.ngrok-free.app';
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://api.yerelhaberbulteni.com";
